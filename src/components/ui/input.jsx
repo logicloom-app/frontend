1295b449e8c39fc5fab477d401dc9fe2488ffe5b
Pakzad <pakzadjs@gmail.com>
2025-03-30 18:58:28 +0200

    Bug fix

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -5,13 +5,13 @@ import { PiEyeBold, PiEyeClosedBold } from "react-icons/pi";
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   const [showPassword, setShowPassword] = useState(false);
-
   const isPasswordType = type === "password";
+  const inputType = isPasswordType && showPassword ? "text" : type;
 
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <input
-        type={isPasswordType && showPassword ? "text" : type}
+        type={inputType}
         autoComplete="off"
         className={cn(
           "flex h-10 w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
@@ -32,6 +32,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
     </div>
   );
 });
+
 Input.displayName = "Input";
 
 export { Input };
